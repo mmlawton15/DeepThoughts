0e1edcb9f5694a1a39ecbdcdad8e9dcc12cb3694
Mary Margaret <mmlawton15@gmail.com>
2022-03-24 18:53:31 -0400

    ginishing up 21.2.5

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const {User, Thought} = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -29,11 +31,25 @@ const resolvers = {
     Mutation: {
       addUser: async (parent, args) => {
         const user = await User.create(args);
+        const token = signToken(user);
       
-        return user;
+        return { token, user };
       },
-      login: async () => {
-
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const token = signToken(user);
+        return { token, user };
       }
     }
 };
