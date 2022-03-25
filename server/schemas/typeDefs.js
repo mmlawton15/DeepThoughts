a4ffa3657798096a8c31a1c67b904053c8664708
Mary Margaret <mmlawton15@gmail.com>
2022-03-24 20:51:08 -0400

    refactoring with lesson code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,5 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
   type User {
     _id: ID
@@ -28,7 +26,13 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
@@ -38,13 +42,10 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-  }
-  
-  type Auth {
-    token: ID!
-    user: User
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
