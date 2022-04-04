157d1e4848fcecdd237472cebf06cb155da00434
Mary Margaret <mmlawton15@gmail.com>
2022-04-04 19:31:13 -0400

    working on 21.3.4 installing appolo client and graph ql. running into issues

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className='flex-column justify-flex-start min-100-vh'>
+      <Header />
+      <div className='container'>
+        <Home />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
