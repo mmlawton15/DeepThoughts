c7f022fd560ffe46a664a17cc24564b933cb0efe
Mary Margaret <mmlawton15@gmail.com>
2022-04-04 20:14:05 -0400

    fixed the bug with some help

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';0
 
 const Home = () => {
+  const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
@@ -8,6 +16,7 @@ const Home = () => {
       </div>
     </main>
   );
+  };
 };
 
 export default Home;
